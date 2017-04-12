442b2fc3b8f4e5f8389214bbd20438e8bc46f0e2
fenicento <stonecoldcrazy@gmail.com>
2017-04-12 16:56:38 +0200

    web socket imported

--- js/common.js
+++ js/common.js
@@ -1,3 +1,18 @@
 /**
  * Created by Cento on 12/04/2017.
  */
+// Let the library know where WebSocketMain.swf is:
+WEB_SOCKET_SWF_LOCATION = "/bower_components/gimite/we-socket-js/WebSocketMain.swf";
+
+// Write your code in the same way as for native WebSocket:
+var ws = new WebSocket("ws://example.com:10081/");
+ws.onopen = function() {
+    ws.send("Hello");  // Sends a message.
+};
+ws.onmessage = function(e) {
+    // Receives a message.
+    alert(e.data);
+};
+ws.onclose = function() {
+    alert("closed");
+};
